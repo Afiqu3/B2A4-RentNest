e31c2b8496c51e9a38b4a77b415bd015d104f3de
Afiqu3 <gahossainj@gmail.com>
2026-07-06 22:18:22 +0600

    Add utility functions for JWT handling, response formatting, and error catching

---
+++ src/utils/catchAsync.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+import httpStatus from "http-status";
+
+export const catchAsync = (fn: RequestHandler) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      fn(req, res, next);
+    } catch (error) {
+      console.log(error);
+
+      next(error);
+    }
+  };
+};
