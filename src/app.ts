fd1e5cd7d3544ff1de5c93a6322e3950c0655f24
Afiqu3 <gahossainj@gmail.com>
2026-07-06 22:23:54 +0600

    Add middleware for error handling and not found routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import config from "./config";
+import { notFound } from "./middlewares/notFound";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 
 const app: Application = express();
 
@@ -19,5 +21,9 @@ app.use(cookieParser());
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
+
+app.use(notFound);
+
+app.use(globalErrorHandler);
 
 export default app;
